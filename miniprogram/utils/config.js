a89ec4ecd4bf19734731a30c720b55108e0c5247
lulu <jamlifephp@gmail.com>
2019-10-28 20:54:58 +0800

    10.28晚

--- miniprogram/utils/config.js
+++ miniprogram/utils/config.js
@@ -6,7 +6,7 @@ var moneyUrl ="https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eo2Pc3pVuWXumNlgMFib5TX
 /**
  * 公众号二维码
  */
-var wechatUrl ="https://test-91f3af.tcb.qcloud.la/common/WechatIMG2.jpeg?sign=e81a38eec6cebfc82c1c34bb7e233bae&t=1556369822"
+var wechatUrl = "https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eo2Pc3pVuWXumNlgMFib5TX3FARXlFXSvcvDPgav2BIfVq5qW6rW9QN430OYSKTXRgpagSLntNp5kw/132"
 
 /**
  * 云开发环境
